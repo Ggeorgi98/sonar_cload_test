b80f6337e40c05e126712a70b0569a509abba895
georgi.gerdzhikov <georgi.gerdzhikov@primeholding.com>
2022-01-26 16:41:46 +0200

    Add more tests

--- SonarRepoTest/Sonar.UnitTests/MathTests.cs
+++ SonarRepoTest/Sonar.UnitTests/MathTests.cs
@@ -13,5 +13,16 @@ namespace Sonar.UnitTests
             var actual = s.Add(a, b);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(2, 3, -1)]
+#pragma warning disable S4144 // Methods should not have identical implementations
+        public void SubTest(int a, int b, int expected)
+#pragma warning restore S4144 // Methods should not have identical implementations
+        {
+            var s = new SimpleMath();
+            var actual = s.Sub(a, b);
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- SonarRepoTest/SonarRepoTest/SimpleMath.cs
+++ SonarRepoTest/SonarRepoTest/SimpleMath.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SonarRepoTest
+﻿namespace SonarRepoTest
 {
     public class SimpleMath
     {
         public int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
         }
     }
 }
